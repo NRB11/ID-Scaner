3dcc808f47f3687e6ee18f8382335b160169845a
NRB11 <34749332+NRB11@users.noreply.github.com>
2023-03-07 11:41:45 +0100

    update

--- Camera.py
+++ Camera.py
@@ -1,6 +1,7 @@
 import cv2
 from pyzbar.pyzbar import decode
 import time as t
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -23,21 +24,17 @@ key = None
 while (key != 27):
     _, frame = cap.read()
     
-    pixel_center = frame[cx, cy]
-    QRCode = pixel_center[0] * 2
-    
-    for code in decode(frame):
-        QRCode = code.data.decode('utf-8')
-        match QRCode in pixel_center[0] * 2:
-            case "4":
-                print("1")
-                t.sleep(5)
-     
+    #r = cv2.selectROI("select the area", frame)
     pixel_center_bgr = frame[cx, cy]
     b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
     #cv2.circle(frame, (cx,cy), 10, (255, 0, 0), 3)
     cv2.rectangle(frame, start_point, end_point, (0, 0, 255), 3)
-    
+    for code in decode(frame):
+        QRCode = code.data.decode('utf-8')  
+        match QRCode:
+            case "4":
+                print("Stoped") 
+   
     cv2.imshow('Camera', frame)
     key = cv2.waitKey(1)
          
--- Main.py
+++ Main.py
@@ -1,72 +1,53 @@
-import customtkinter
-import tkinter
+import cv2
 
-customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
-#customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+videoCaptureObject = cv2.VideoCapture(0)
 
-app = customtkinter.CTk()
-app.geometry("400x780")
-app.title("CustomTkinter simple_example.py")
+def sketch_transform(image):  # in here you can do image filteration
+    image_grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_grayscale_blurred = cv2.GaussianBlur(image_grayscale, (7,7), 0)
+    image_canny = cv2.Canny(image_grayscale_blurred, 10, 80)
+    _, mask = image_canny_inverted = cv2.threshold(image_canny, 30, 255, cv2.THRESH_BINARY_INV)
+    return mask
 
-def button_callback():
-    print("Button click", combobox_1.get())
+upper_left = (50, 50)
+bottom_right = (300, 300)
 
+result = True
+while(result):
+    ret,image_frame = videoCaptureObject.read()
 
-def slider_callback(value):
-    progressbar_1.set(value)
+    #Rectangle marker
+    r = cv2.rectangle(image_frame, upper_left, bottom_right, (100, 50, 200), 5)
+    rect_img = image_frame[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]]
+    
+    sketcher_rect = rect_img
+    sketcher_rect = sketch_transform(sketcher_rect)
+    
+    #Conversion for 3 channels to put back on original image (streaming)
+    sketcher_rect_rgb = cv2.cvtColor(sketcher_rect, cv2.COLOR_GRAY2RGB)
+    
+    #Replacing the sketched image on Region of Interest
+    image_frame[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]] = sketcher_rect_rgb
+    
+    cv2.imshow("test", image_frame)
+    k = cv2.waitKey(1)
 
+    if k%256 == 27:
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        cv2.imwrite("input_image.jpg",image_frame)
+        img = cv2.imread("input_image.jpg")
 
-frame_1 = customtkinter.CTkFrame(master=app)
-frame_1.pack(pady=20, padx=60, fill="both", expand=True)
+        # you can do those function inside the sketch_transform def
 
-label_1 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT)
-label_1.pack(pady=10, padx=10)
+        #gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+        #gray_img = cv2.resize(gray_image, (28, 28)).reshape(1,28,28,1)
+        #ax[2,1].imshow(gray_img.reshape(28, 28) , cmap = "gray")
+        #cv2.imshow("image", gray_img.reshape(28, 28))
 
-progressbar_1 = customtkinter.CTkProgressBar(master=frame_1)
-progressbar_1.pack(pady=10, padx=10)
-
-button_1 = customtkinter.CTkButton(master=frame_1, command=button_callback)
-button_1.pack(pady=10, padx=10)
-
-slider_1 = customtkinter.CTkSlider(master=frame_1, command=slider_callback, from_=0, to=1)
-slider_1.pack(pady=10, padx=10)
-slider_1.set(0.5)
-
-entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="CTkEntry")
-entry_1.pack(pady=10, padx=10)
-
-optionmenu_1 = customtkinter.CTkOptionMenu(frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
-optionmenu_1.pack(pady=10, padx=10)
-optionmenu_1.set("CTkOptionMenu")
-
-combobox_1 = customtkinter.CTkComboBox(frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
-combobox_1.pack(pady=10, padx=10)
-combobox_1.set("CTkComboBox")
-
-checkbox_1 = customtkinter.CTkCheckBox(master=frame_1)
-checkbox_1.pack(pady=10, padx=10)
-
-radiobutton_var = customtkinter.IntVar(value=1)
-
-radiobutton_1 = customtkinter.CTkRadioButton(master=frame_1, variable=radiobutton_var, value=1)
-radiobutton_1.pack(pady=10, padx=10)
-
-radiobutton_2 = customtkinter.CTkRadioButton(master=frame_1, variable=radiobutton_var, value=2)
-radiobutton_2.pack(pady=10, padx=10)
-
-switch_1 = customtkinter.CTkSwitch(master=frame_1)
-switch_1.pack(pady=10, padx=10)
-
-text_1 = customtkinter.CTkTextbox(master=frame_1, width=200, height=70)
-text_1.pack(pady=10, padx=10)
-text_1.insert("0.0", "CTkTextbox\n\n\n\n")
-
-segmented_button_1 = customtkinter.CTkSegmentedButton(master=frame_1, values=["CTkSegmentedButton", "Value 2"])
-segmented_button_1.pack(pady=10, padx=10)
-
-tabview_1 = customtkinter.CTkTabview(master=frame_1, width=200, height=70)
-tabview_1.pack(pady=10, padx=10)
-tabview_1.add("CTkTabview")
-tabview_1.add("Tab 2")
-
-app.mainloop()
+        y_pred = model.predict_classes(img)
+        print("predicted alphabet  = ", y_pred) 
+        #text_to_audio(myDict.get(y_pred[0]))   
+videoCaptureObject.release()
+cv2.destroyAllWindows()
